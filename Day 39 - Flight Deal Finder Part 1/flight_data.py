6ebfc6b1fc27243f3a8530876515607691d2cf1e
Vellister <102322267+Vellister@users.noreply.github.com>
2024-11-15 19:38:30 -0300

    Create flight_data.py

---
+++ Day 39 - Flight Deal Finder Part 1/flight_data.py
@@ -0,0 +1,14 @@
+class FlightData:
+    def __init__(self, flight):
+        self.data = flight[0]
+        self.formatted_data = self._format_data()
+
+    def _format_data(self):
+        return {
+            'destiny': self.data['cityTo'],
+            'from': f'{self.data["cityFrom"]}-{self.data["flyFrom"]}',
+            'to': f'{self.data["cityTo"]}-{self.data["flyTo"]}',
+            'price': self.data['price'],
+            'departure_date': self.data['route'][0]["local_departure"].split('T')[0],
+            'arrival_date': self.data['route'][1]["local_departure"].split('T')[0]
+        }
