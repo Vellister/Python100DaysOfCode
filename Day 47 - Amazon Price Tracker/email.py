c4361c307528bc970889c362e0c0c8390957bf39
Vellister <102322267+Vellister@users.noreply.github.com>
2024-12-02 18:45:20 -0300

    Create email.py

---
+++ Day 47 - Amazon Price Tracker/email.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+
+# Practice
+url = "https://appbrewery.github.io/instant_pot/"
+# Live Site
+# url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+# print(soup.prettify())
+
+# Find the HTML element that contains the price
+price = soup.find(class_="a-offscreen").get_text()
+
+# Remove the dollar sign using split
+price_without_currency = price.split("$")[1]
+
+# Convert to floating point number
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+# ====================== Send an Email ===========================
+
+# Get the product title
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+# Set the price below which you would like to get a notification
+BUY_PRICE = 100
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
