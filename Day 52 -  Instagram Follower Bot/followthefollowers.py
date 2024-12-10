d9370c85a9485e9b60b60ea6ec3d44fab95aa6ca
Vellister <102322267+Vellister@users.noreply.github.com>
2024-12-10 16:15:02 -0300

    Create followthefollowers.py

---
+++ Day 52 -  Instagram Follower Bot/followthefollowers.py
@@ -0,0 +1,14 @@
+from selenium.common.exceptions import ElementClickInterceptedException
+
+    def follow(self):
+        # Check and update the (CSS) Selector for the "Follow" buttons as required. 
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, value='._aano button')
+
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(1.1)
+            # Clicking button for someone who is already being followed will trigger dialog to Unfollow/Cancel
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element(by=By.XPATH, value="//button[contains(text(), 'Cancel')]")
+                cancel_button.click()
