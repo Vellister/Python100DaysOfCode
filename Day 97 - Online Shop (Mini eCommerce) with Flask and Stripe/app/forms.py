09f3b38f6f2eb3153ed00f38b9ca2c5beb292594
Vellister <102322267+Vellister@users.noreply.github.com>
2025-10-31 15:09:11 -0300

    Create forms.py

---
+++ Day 97 - Online Shop (Mini eCommerce) with Flask and Stripe/app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmar Senha',
+                                     validators=[DataRequired(), EqualTo('password', message='As senhas devem ser iguais.')])
+    submit = SubmitField('Cadastrar')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Este email já está em uso. Por favor, escolha outro.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember = BooleanField('Lembrar-me')
+    submit = SubmitField('Login')
